e2e31bc18861616fb6edfd55b0e6f78fc096f1ea
Lucas Raiony <raionylucas7@gmail.com>
2024-08-03 11:37:11 -0300

    adicionando novo projeto

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -6,12 +6,18 @@ function Projects() {
     <section className='projects layout-grid'>
       <h2>Projetos</h2>
       <div className='projects__cards'>
+        <CardProject 
+        name='Gerenciador Financeiro'
+        description = ' Sistema desenvolvido para gerenciar finanças pessoais desenvolvido com PHP, MySQL, Composer e Padrão MVC.' 
+        projectLink='https://github.com/raionylucas/gerenciador-financeiro-php/tree/main'
+        textBtnProject ='Visitar Repositório'
+        />
         <CardProject
           name='Data Agency'
           description='Landing page responsiva desenvolvida utilizando HTML, CSS e JavaScript.'
           projectLink='https://dataagency.vercel.app'
+          textBtnProject ='Visualizar Projeto'
         />
-        <CardProject />
         <CardProject />
       </div>
     </section>
